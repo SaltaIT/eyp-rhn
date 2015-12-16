4be14a41f957dee02834b5d523238dde2b548166
Jordi Prats <jprats@systemadmin.es>
2015-12-16 14:27:28 +0100

    inicial

---
+++ manifests/init.pp
@@ -0,0 +1,55 @@
+# == Class: rhn
+#
+# Full description of class rhn here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { 'rhn':
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+# Copyright 2015 Your name here, unless otherwise noted.
+#
+class rhn($username, $password) inherits rhn::params {
+
+  Exec {
+    path => '/usr/sbin:/usr/bin:/sbin:/bin',
+  }
+
+  package { $rhn::params::packages:
+    ensure => 'installed',
+  }
+
+  exec { 'rhn register':
+    command => "rhnreg_ks '--username=${username}' '--password=${password}'",
+    unless  => 'rhn_check',
+    require => Package[$rhn::params::packages],
+  }
+
+
+}
---
+++ manifests/params.pp
@@ -0,0 +1,27 @@
+class rhn::params {
+
+	case $::osfamily
+	{
+    'redhat':
+    {
+      case $::operatingsystem
+      {
+        'RedHat':
+        {
+          $packages= [ 'rhn-setup', 'rhn-check' ]
+        }
+        default:
+        {
+          notify{ 'rhn':
+      				message => "Nothing to to here -_-",
+      		}
+        }
+      }
+    }
+		'Debian':
+		{
+      fail('Nothing to to here')
+		}
+		default: { fail('Nothing to to here')  }
+	}
+}
